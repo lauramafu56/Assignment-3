e3d657404d54ae4cfe9f2265fe0f3e232aff1f48
Popplio56 <andreamafu56@gmail.com>
2026-04-16 16:59:55 +0200

    HE DEFINIDO ALGUNAS VARIABLES EN EL PRIMER MODEL PARA EXTRAER LA DATA DEL JSON

---
+++ Models/FLIGHT_TAKE_DATA.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Assignment3;
+
+public class FLIGHT_TAKE_DATA
+{
+    public string FLIGHTNUMBER { get; set;}= string.Empty;
+    public string ARLINENAME {get; set;}= string.Empty;
+
+    
+
+
+}
