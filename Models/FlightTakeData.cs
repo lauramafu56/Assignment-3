665c983474c5e5aefc7db2dea4e8e60eb9dafdb2
lauramafu56 <lauramafu56@gmail.com>
2026-04-17 21:54:58 +0200

    no queremos esto, volver al Main.

---
+++ Models/FlightTakeData.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Assignment3;
+
+public class FlightTakeData
+{
+    public string FLIGHTNUMBER { get; set;}= string.Empty;
+    public string ARLINENAME {get; set;}= string.Empty;
+     public string DEPARTUREAIRPORT {get; set;}= string.Empty;
+     public string AIRLINECODE{get; set;}= string.Empty;
+     public string ARRIVALAIRPORT {get; set;}= string.Empty;
+     public string SCHEDULEDDEPARTURE {get; set;}= string.Empty;
+     public string SCHEDULEDARRIVAL {get; set;}= string.Empty;
+     public string AIRCRAFTTYPE {get; set;}= string.Empty;
+     public string STATUS {get; set;}= string.Empty; 
+
+}
