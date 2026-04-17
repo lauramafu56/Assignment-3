36a403da389589cef06e6ebb8913dfc7dd7635a9
Popplio56 <andreamafu56@gmail.com>
2026-04-17 21:50:50 +0200

    creacion de airport take data

---
+++ Models/AirportTakeData.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Assignment3;
+
+public class AirportTakeData
+{
+    public string IATACODE { get; set;}= string.Empty;
+    public string NAME {get; set;}= string.Empty;
+     public string CITY {get; set;}= string.Empty;
+     public string COUNTRY{get; set;}= string.Empty;
+     public double LATITUDE {get; set;}= 0.0;
+     public double LONGITUDE {get; set;}= 0.0;
+
+}
