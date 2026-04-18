d3e17f593d9d9f24adc0c97f80ae093366256179
Popplio56 <andreamafu56@gmail.com>
2026-04-18 21:25:00 +0200

    intento de mejorar la vista con el mapa

--- obj/Debug/net10.0/Assignment3.AssemblyInfo.cs
+++ obj/Debug/net10.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b8ecec147ebb3b64fe39f3b7380e1ba2b9c5ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9417b7861852d366872dd2c4c762516a2c9833e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
