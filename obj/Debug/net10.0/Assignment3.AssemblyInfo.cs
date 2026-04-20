9e9b734c7b6056d6526ecde90d7699d43b10e617
lauramafu56 <lauramafu56@gmail.com>
2026-04-20 20:22:26 +0200

    basura

--- obj/Debug/net10.0/Assignment3.AssemblyInfo.cs
+++ obj/Debug/net10.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b575ba245d6daeebb6f9b0fcb723207425702250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef0080cc2049a812cf149137dc993673acf64b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
