86a980122f3e55a4b0a9dcb043b917dba545241d
lauramafu56 <lauramafu56@gmail.com>
2026-04-20 20:24:30 +0200

    trying to get the good one branch

--- obj/Debug/net10.0/Assignment3.AssemblyInfo.cs
+++ obj/Debug/net10.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e2da0a88cee06bb6861231dd0f2b8f438aa05d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0899061d55f7ade1155a77e442dbfcbcd71debc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
