3e8e1a7e504a0c156c7de1b99c011500cace5a42
Popplio56 <andreamafu56@gmail.com>
2026-04-29 10:38:36 +0200

    WORKING 100 PERCENT WITH BONUS POINT

--- ViewModels/ThirdViewModel.cs
+++ ViewModels/ThirdViewModel.cs
@@ -17,6 +17,7 @@ using LiveChartsCore.SkiaSharpView.Drawing;
 
 
 using LiveChartsCore.SkiaSharpView.SKCharts;
+using BruTile.Wmts.Generated;
 
 
 namespace Assignment3.ViewModels;
@@ -27,15 +28,27 @@ public partial class ThirdViewModel: ObservableObject
    
     private List<Airport> Airports = new();
     private List<Flights> Flights= new();
+    //graph 1
     public ISeries[] Series { get; set; }
     public Axis[] XAxes { get; set; }
-    
+    public Axis[] YAxes { get; set; }
+    //graph 2
     public ISeries[] Series2 { get; set; }
     public Axis[] XAxes2 { get; set; }
     public Axis[] YAxes2 { get; set; }
+    //Pie graph
     public ObservableCollection<ISeries> PieSeries { get; set; } = new();
+    //duration graph
     public ObservableCollection<ISeries> DurationSeries { get; set; } = new();
     public Axis[] DurationXAxes { get; set; }
+    // graph 3
+     public ISeries[] Series3 { get; set; }
+    public Axis[] XAxes3 { get; set; }
+    public Axis[] YAxes3 { get; set; }
+    //graph 4
+    public ISeries[] Series4 { get; set; }
+    public Axis[] XAxes4 { get; set; }
+    public Axis[] YAxes4 { get; set; }
 
     public ObservableCollection<Flights> AvailableFlights {get; set;}= new();
 
@@ -51,6 +64,10 @@ private bool _showSeries2 = true; //
 private bool _showPieSeries = true; // 
 [ObservableProperty]
 private bool _showDurationSeries = false; //
+[ObservableProperty]
+private bool _showSeries3 = false; //
+[ObservableProperty]
+private bool _showSeries4 = false; //
 
 [ObservableProperty]
 private bool _showEmptyState;
@@ -81,8 +98,11 @@ private bool _showEmptyState;
             if (HiddenCharts.Contains("Average Duration"))
                 chartsList.Add("Average Duration");
 
-            if (HiddenCharts.Contains("Top Artists by Listeners"))
-                chartsList.Add("Top Artists by Listeners");
+            if (HiddenCharts.Contains("Airports graph"))
+                chartsList.Add("Airports graph");
+
+            if (HiddenCharts.Contains("the last chart"))
+                chartsList.Add("the last chart");
                 
             return chartsList;
         }
@@ -134,6 +154,16 @@ public void RemoveChart(string chartId)
             ShowDurationSeries = false;
             chartToRemove = "Average Duration";
             break;
+          case "AirportSeries":
+            ShowSeries3 = false;
+            chartToRemove = "Airports graph";
+            break;
+          case "Lastone":
+            ShowSeries4 = false;
+            chartToRemove = "the last chart";
+            break;
+       
+
     }
 
     if (chartToRemove != null)
@@ -157,19 +187,71 @@ public void AddChart(string chartName)
        CalcularDuraciones(data.Flights);
         
         ShowDurationSeries = true; // ¡Aquí ocurre la magia!
+        OnPropertyChanged(nameof(ShowDurationSeries));
+        return;
     }
-    else if (chartName == "Show All Charts")
-    {
-        ShowSeries = true; 
-        ShowSeries2 = true; 
-        ShowPieSeries = true; 
-        ShowDurationSeries = true;
-    }
+    if (chartName == "Top Airlines")
+        {
+            // Show the specified charts
+            ShowSeries = true;
+
+            // Update available charts
+            OnPropertyChanged(nameof(AvailableCharts));
+              
+            HiddenCharts.Remove("Top Airlines"); 
+           
+            return;
+        }
+       if (chartName == "Flights per City")
+        {
+            // Show the specified charts
+            ShowSeries2 = true;
+
+            // Update available charts
+            OnPropertyChanged(nameof(AvailableCharts));
+              
+            HiddenCharts.Remove("Flights per City"); 
+           
+            return;
+        }
+     if (chartName == "Flight Status")
+        {
+            // Show the specified charts
+            ShowPieSeries = true;
+
+            // Update available charts
+            OnPropertyChanged(nameof(AvailableCharts));
+              
+            HiddenCharts.Remove("Flight Status"); 
+           
+            return;
+        }
+    
+    
+   if (chartName == "Show All Charts")
+        {
+            // Show all charts
+            ShowSeries3 = true;
+            ShowDurationSeries = true;
+            ShowSeries4= true;
+            
+            // Hide empty state since charts are now visible
+            ShowEmptyState = false;
+            
+            // Update available charts
+            OnPropertyChanged(nameof(AvailableCharts));
+            
+             HiddenCharts.Remove("Average Duration"); 
+             HiddenCharts.Remove("Airports graph"); 
+             HiddenCharts.Remove("the last chart"); 
+            return;
+        }
     
     // Quitamos la opción de la lista de "pendientes"
-    HiddenCharts.Remove(chartName);
+    
+   
       // Notificamos manualmente por si acaso
-    OnPropertyChanged(nameof(ShowDurationSeries));
+   
     OnPropertyChanged(nameof(AvailableCharts));
     
   
@@ -202,6 +284,7 @@ public void AddChart(string chartName)
             {
                 Values = topAirlines.Select(a => a.Total).ToArray(),
                 Name = "Total flights",
+                Fill= new SolidColorPaint(SKColors.LightGreen),
                 
             }
             
@@ -215,6 +298,14 @@ public void AddChart(string chartName)
             LabelsRotation = 25, // To read better if the names are long //Para que se lean mejor si los nombres son largos
 
         }
+        };
+         YAxes = new Axis[]
+        {
+            new Axis
+            {
+                Name="Number of flights",
+               
+            }
         };
 
         Flights = data3.Flights;
@@ -234,7 +325,8 @@ public void AddChart(string chartName)
             new ColumnSeries<int>
             {
                 Values = topCountrys.Select(a => a.TotalFlights).ToArray(),
-                Name = "Number of flights per city"
+                Name = "Number of flights per city",
+                Fill= new SolidColorPaint(SKColors.Orange),
             }
         };
 
@@ -280,13 +372,13 @@ public void AddChart(string chartName)
     var promedioPorDestino = data.Flights
         .Select(f => {
             // Intentamos convertir las strings a DateTime
-           DateTime salida = f.ScheduledDeparture;
-            DateTime llegada = f.ScheduledArrival;
+           DateTime Departure = f.ScheduledDeparture;
+            DateTime Arrival = f.ScheduledArrival;
             
             // Si la llegada es "menor" que la salida, es que aterrizó al día siguiente
-            if (llegada < salida) llegada = llegada.AddDays(1);
+            if (Arrival < Departure) Arrival = Arrival.AddDays(1);
             
-            return new { f.ArrivalAirport, Duracion = (llegada - salida).TotalMinutes };
+            return new { f.ArrivalAirport, Duracion = (Arrival - Departure).TotalMinutes };
         })
         .GroupBy(x => x.ArrivalAirport)
         .Select(g => new { 
@@ -308,8 +400,100 @@ public void AddChart(string chartName)
         new Axis { Labels = promedioPorDestino.Select(d => d.Destino).ToArray(),
         }
     };
+
     HiddenCharts.Add("Average Duration"); 
+    HiddenCharts.Add("Airports graph"); 
+     HiddenCharts.Add("the last chart"); 
     OnPropertyChanged(nameof(AvailableCharts));
+
+
+    Flights = data3.Flights;
+         var top = Flights
+            .GroupBy(f => f.DepartureAirport)// we make groups by name of the city
+            .Select(group3 => new { //we select the name 
+                NameOfCountry = group3.Key, 
+                TotalCountrys = group3.Count() //we count how much fligths are there
+            })
+
+            .OrderByDescending(x => x.TotalCountrys)// we order from biggest to smallest
+            .Take(6)//we take the 6 first for not make the graph ugly
+            .ToList();
+
+        Series3 = new ISeries[]
+        {
+            new ColumnSeries<int>
+            {
+                Values = top.Select(d => d.TotalCountrys).ToArray(),
+                Name = "AirportSeries",
+                Fill= new SolidColorPaint(SKColors.Purple),
+                
+            }
+            
+        };
+
+        XAxes3 = new Axis[]
+        {
+        new Axis
+        {
+            Labels = top.Select(c => c.NameOfCountry).ToArray(),
+            LabelsRotation = 25, // To read better if the names are long //Para que se lean mejor si los nombres son largos
+
+        }
+        };
+         YAxes3 = new Axis[]
+        {
+            new Axis
+            {
+                Name="Number of flights",
+               
+            }
+        };
+
+          Flights = data3.Flights;
+
+
+        var topDayOfTheWeek = Flights
+            .GroupBy(f => f.ScheduledDeparture.DayOfWeek)// we make groups by the departure time of the airline
+            .Select(group4 => new { //we select the name 
+                NameOfTheDay = group4.Key.ToString(),
+                TotalD = group4.Count() //we count how much fligths are there
+            })
+            .OrderByDescending(x => x.TotalD)// we order from biggest to smallest
+            //.Take(7)//we take the 7 first for not make the graph ugly
+            .ToList();
+
+            
+        Series4 = new ISeries[]
+        {
+            new ColumnSeries<int>
+            {
+                Values = topDayOfTheWeek.Select(b => b.TotalD).ToArray(),
+                Name = "AirportSeries",
+                Fill= new SolidColorPaint(SKColors.Salmon),
+                
+            }
+            
+        };
+        
+
+        XAxes4 = new Axis[]
+        {
+        new Axis
+        {
+            
+            Labels = topDayOfTheWeek.Select(b => b.NameOfTheDay).ToArray(),
+            LabelsRotation = 25, // To read better if the names are long //Para que se lean mejor si los nombres son largos
+
+        }
+        };
+         YAxes4 = new Axis[]
+        {
+            new Axis
+            {
+                Name="Number of flights",
+               
+            }
+        };
 
     }
 
@@ -336,6 +520,7 @@ public void AddChart(string chartName)
     {
         Values = promedioData.Select(d => d.Promedio).ToArray(),
         Name = "Minutes Average",
+        Fill= new SolidColorPaint(SKColors.Pink),
         DataLabelsPosition = LiveChartsCore.Measure.DataLabelsPosition.End,
         DataLabelsFormatter = point => $"{point.Coordinate.PrimaryValue} min"
     });
